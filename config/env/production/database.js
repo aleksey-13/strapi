53ceea8449dcad5e5b6c92e2ea5d9e71f6c817c2
unknown <alekseymagadan@gmail.com>
2023-01-11 12:27:23 +0200

    push

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,17 +1,15 @@
+const fs = require("fs");
 module.exports = ({ env }) => ({
   connection: {
     client: "mysql",
     connection: {
-      //   socketPath: env("INSTANCE_UNIX_SOCKET"),
-      //   database: env("DB_NAME"),
-      //   user: env("DB_USER"),
-      //   password: env("DB_PASS"),
-      host: env("DATABASE_HOST", "34.66.68.94"),
-      port: env.int("DATABASE_PORT", 3306),
-      database: env("DATABASE_NAME", "strapi-test-db"),
-      user: env("DATABASE_USERNAME", "hose"),
-      password: env("DATABASE_PASSWORD", "1q2w3e4r"),
-      ssl: env.bool("DATABASE_SSL", true),
+      host: process.env.DATABASE_HOST || "34.66.68.94",
+      port: process.env.DATABASE_PORT || 5432,
+      database: process.env.DATABASE_NAME || "strapi-test-db",
+      user: process.env.DATABASE_USERNAME || "hose",
+      password: process.env.DATABASE_PASSWORD || "1q2w3e4r",
+      ssl: env.bool("DATABASE_SSL", false),
     },
+    debug: false,
   },
 });
