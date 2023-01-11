bf5c990b2d3f8677f87987ca0adb7ff24a70d8bf
unknown <alekseymagadan@gmail.com>
2023-01-11 12:40:23 +0200

    updatye

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -4,11 +4,11 @@ module.exports = ({ env }) => ({
     client: "mysql",
     connection: {
       host: process.env.DATABASE_HOST || "34.66.68.94",
-      port: process.env.DATABASE_PORT || 5432,
+      port: process.env.DATABASE_PORT || 3306,
       database: process.env.DATABASE_NAME || "strapi-test-db",
       user: process.env.DATABASE_USERNAME || "hose",
       password: process.env.DATABASE_PASSWORD || "1q2w3e4r",
-      ssl: env.bool("DATABASE_SSL", false),
+      ssl: env.bool("DATABASE_SSL", true),
     },
     debug: false,
   },
