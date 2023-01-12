e3e930549b8746c936d15c4aef2c189d6bd935e5
unknown <alekseymagadan@gmail.com>
2023-01-12 10:56:23 +0200

    add ssl

--- config/database.js
+++ config/database.js
@@ -1,13 +1,25 @@
+const fs = require("fs");
+
 module.exports = ({ env }) => ({
   connection: {
-    client: 'mysql',
+    client: "mysql",
     connection: {
-      host: env('DATABASE_HOST', '34.66.68.94'),
-      port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'strapi-test-db'),
-      user: env('DATABASE_USERNAME', 'hose'),
-      password: env('DATABASE_PASSWORD', '1q2w3e4r'),
-      ssl: env.bool('DATABASE_SSL', true),
+      host: env("DATABASE_HOST", "34.66.68.94"),
+      port: env.int("DATABASE_PORT", 3306),
+      database: env("DATABASE_NAME", "strapi-test-db"),
+      user: env("DATABASE_USERNAME", "hose"),
+      password: env("DATABASE_PASSWORD", "1q2w3e4r"),
+      // ssl: env.bool('DATABASE_SSL', true),
+      ssl: {
+        ca: fs.readFileSync("./config/env/production/server-ca.pem").toString(),
+        cert: fs
+          .readFileSync("./config/env/production/client-cert.pem")
+          .toString(),
+        key: fs
+          .readFileSync("./config/env/production/client-key.pem")
+          .toString(),
+        rejectUnauthorized: false,
+      },
     },
   },
 });
--- config/env/production/database.js
+++ config/env/production/database.js
@@ -8,7 +8,17 @@ module.exports = ({ env }) => ({
       database: process.env.DATABASE_NAME || "strapi-test-db",
       user: process.env.DATABASE_USERNAME || "hose",
       password: process.env.DATABASE_PASSWORD || "1q2w3e4r",
-      ssl: env.bool("DATABASE_SSL", true),
+      //   ssl: env.bool("DATABASE_SSL", true),
+      ssl: {
+        ca: fs.readFileSync("./config/env/production/server-ca.pem").toString(),
+        cert: fs
+          .readFileSync("./config/env/production/client-cert.pem")
+          .toString(),
+        key: fs
+          .readFileSync("./config/env/production/client-key.pem")
+          .toString(),
+        rejectUnauthorized: false,
+      },
     },
     debug: false,
   },
